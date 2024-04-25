e88fe719867fa78fc2b490a086b5e2e2d2a9f018
Vishrut Jha <vishrutkmr7@icloud.com>
2024-04-25 03:09:04 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vishrut Jha on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
